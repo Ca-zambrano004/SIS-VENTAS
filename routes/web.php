c23d50f2de20e715f6bafebbd68d173f40fbbefd
carlos <carloszambranolamaar@hotmail.com>
2020-05-08 13:31:43 -0500

    Avance

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::post('/cart', 'CartDetailController@store'); //gestionar carrito de compr
 Route::delete('/cart', 'CartDetailController@destroy'); //elim. produ. de carrito de compras
 
 Route::post('/orden', 'CartController@update'); //gestionar compras
+Route::post('/orden', 'CartController@update'); //gestionar cosañslas
 
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->namespace('Admin')->group(function () {

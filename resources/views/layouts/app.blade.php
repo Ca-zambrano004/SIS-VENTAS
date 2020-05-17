d1f4942fa16e3989171b48008b846288c090a24e
carlos <carloszambranolamaar@hotmail.com>
2020-05-17 14:20:50 -0500

    cambios2.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,138 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="es">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset ('assets/img/pintura.png') }}">
+  <link rel="icon" type="image/png" href="{{ asset ('assets/img/pintura.png') }}">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    @yield('title', 'Aeroshop')
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons   -->  
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href=" {{ asset ('assets/css/material-kit.css') }}" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href=" {{ asset ('assets/demo/demo.css') }}" rel="stylesheet" />
+  @yield ('estilos')
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<body class="@yield('body-class')">
+ <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="{{ url('/')}}">
+         <!-- <img src="{{ asset ('assets/img/Aeromat.gif') }}" alt="Aeromat" class="img-responsive" width="70%">-->
+          <font SIZE=16 FACE="times new roman"><b>Aeroshop</b></font>
+       </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+     
+          @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}"> {{ __('INGRESAR') }} </a>
+            </li>
+            
+          @if (Route::has('register'))
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}"> {{ __('REGISTRO') }} </a>
+            </li>
+          @endif
 
-                    </ul>
+          @else
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+             Menú de Usuario<span class="caret"></span>
+            </a>
+              <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  
+                  <li>
+                      <a class="dropdown-item" href="{{ url('/home') }}">
+                         Home
+                      </a>
+                  </li>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                  <li>
+                      <a class="dropdown-item" href="{{ url('/productos') }}">
+                         Ver Productos
+                      </a>
+                  </li>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                  @if(auth()->user()->rol)
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+                  <li>
+                      <a class="dropdown-item" href="{{ url('/admin/productos') }}">
+                         Gestionar Productos
+                      </a>
+                  </li>
+                  <li>
+                      <a class="dropdown-item" href="{{ url('/admin/categorias') }}">
+                         Gestionar Categorias
+                      </a>
+                  </li>
+                 
+                  @endif
+                  <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        {{ __('Salir') }}
+                      </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                      </form>
+                  </li>
+                  
+              </ul>
+            </li>
+            <li class="nav-item">
+                 <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="{{url('/cuenta') }}"data-original-title="{{ Auth::user()->name }}"> 
+                  <img src="{{ asset ('assets/img/avatar.png') }}" alt="Imagen principal de la categoria" class="rounded" class="img-responsive" width="50">    
+                 </a>
+            </li>
+          @endguest
+          <li class="nav-item">
+             <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="{{url('/contacto') }}" data-original-title="Contacto"> {{ __('Contacto') }}</a>
+          </li> 
+        </ul>
+      </div>
     </div>
-</body>
+  </nav>
+    <div class="wrapper">
+
+      @yield ('content')
+    
+    </div>
+
+ </body>
+  <!--   Core JS Files   -->
+  <script src="{{ asset ('assets/js/core/jquery.min.js') }}" type="text/javascript"></script>
+  <script src="{{ asset ('assets/js/core/popper.min.js') }}" type="text/javascript"></script>
+  <script src="{{ asset ('assets/js/core/bootstrap-material-design.min.js') }}" type="text/javascript"></script>
+  <script src="{{ asset ('assets/js/plugins/moment.min.js') }}"></script>
+
+  <!--  Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+  <script src="{{ asset ('assets/js/plugins/bootstrap-datetimepicker.js') }}" type="text/javascript"></script>
+  
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="{{ asset ('assets/js/plugins/nouislider.min.js') }}" type="text/javascript"></script>
+  
+  <!--  Google Maps Plugin    -->
+  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+  <script src="{{ asset ('assets/js/material-kit.js') }}" type="text/javascript"></script>
+  
+ 
+
 </html>
